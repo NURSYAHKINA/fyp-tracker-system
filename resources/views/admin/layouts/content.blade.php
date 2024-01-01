96bd71a8dd0e943868f54353243507d510f2a96e
Muhammad Taufiq Bin Mohd Jaslan <105698083+taufiqjaslan@users.noreply.github.com>
2024-01-01 17:54:03 +0800

    fix calender dashboard UI

--- resources/views/admin/layouts/content.blade.php
+++ resources/views/admin/layouts/content.blade.php
@@ -1,103 +1,106 @@
-       <!--Content Page-->
-        <div class="main-content">
-            <div class="container-fluid">
+<div class="main-content">
+    <div class="container-fluid">
 
-                <!--All Card-->
-                <div class="row clearfix">
+        <!-- All Card -->
+        <div class="row clearfix">
 
-                    <!--Supervisor Card-->
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <div class="widget">
-                            <div class="widget-body">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="state">
-                                        <h6>Supervisor</h6>
-                                        <h2>23</h2>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ik ik-user-check"></i>
-                                    </div>
-                                </div>
-                                <small class="text-small mt-10 d-block">6% higher than last month</small>
+            <!-- Supervisor Card -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget">
+                    <div class="widget-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="state">
+                                <h6>Supervisor</h6>
+                                <h2>23</h2>
+                            </div>
+                            <div class="icon">
+                                <i class="ik ik-user-check"></i>
                             </div>
                         </div>
-                    </div>
-
-                    <!--Student Card-->
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <div class="widget">
-                            <div class="widget-body">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="state">
-                                        <h6>Student</h6>
-                                        <h2>226</h2>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ik ik-user"></i>
-                                    </div>
-                                </div>
-                                <small class="text-small mt-10 d-block">61% higher than last month</small>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!--Feedback Card-->
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <div class="widget">
-                            <div class="widget-body">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="state">
-                                        <h6>Feedback</h6>
-                                        <h2>52</h2>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ik ik-send"></i>
-                                    </div>
-                                </div>
-                                <small class="text-small mt-10 d-block">Total Events</small>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!--Report Card-->
-                    <div class="col-lg-3 col-md-6 col-sm-12">
-                        <div class="widget">
-                            <div class="widget-body">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="state">
-                                        <h6>Report</h6>
-                                        <h2>23</h2>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ik ik-clipboard"></i>
-                                    </div>
-                                </div>
-                                <small class="text-small mt-10 d-block">Total Comments</small>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <!--Calendar Card-->
-                <div class="col-md-7">
-                    <div class="card">
-                        <div class="card-body">
-                            <div id='calendar'>
-                            </div>
-                        </div>
+                        <small class="text-small mt-10 d-block">6% higher than last month</small>
                     </div>
                 </div>
+            </div>
 
-                <!--Donut Chart-->
-                <div class="col-md-4">
-                    <div class="card" style="min-height: 422px;">
-                        <div class="card-header">
-                            <h3>Student chart</h3>
+            <!-- Student Card -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget">
+                    <div class="widget-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="state">
+                                <h6>Student</h6>
+                                <h2>226</h2>
+                            </div>
+                            <div class="icon">
+                                <i class="ik ik-user"></i>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <div id="c3-donut-chart"></div>
+                        <small class="text-small mt-10 d-block">61% higher than last month</small>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Feedback Card -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget">
+                    <div class="widget-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="state">
+                                <h6>Feedback</h6>
+                                <h2>52</h2>
+                            </div>
+                            <div class="icon">
+                                <i class="ik ik-send"></i>
+                            </div>
+                        </div>
+                        <small class="text-small mt-10 d-block">Total Events</small>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Report Card -->
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget">
+                    <div class="widget-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="state">
+                                <h6>Report</h6>
+                                <h2>23</h2>
+                            </div>
+                            <div class="icon">
+                                <i class="ik ik-clipboard"></i>
+                            </div>
+                        </div>
+                        <small class="text-small mt-10 d-block">Total Comments</small>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row clearfix">
+            <!-- Calendar Card -->
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-body">
+                        <div id='calendar'>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Donut Chart -->
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Student chart</h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="c3-donut-chart"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
